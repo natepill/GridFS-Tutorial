a5c2a9412cb97c9fd115e7b3557ebb847ad700f7
Nathan Pillai <natepill@gmail.com>
2019-03-14 16:52:57 -0700

    Grid FS multer set up middleware in storage variable

--- app.js
+++ app.js
@@ -1,9 +1,68 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 const port = 5000
 
+// Initialize grid fs stream
+let gfs;
 
+
+//Middleware
+app.use(bodyParser.json())
+app.use(methodOverride('_method')) //Telling method override that we want to use a query string when we create our form in order to make a delete request
 app.set('view engine', 'ejs');
+
+//Mongo URI
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/gridfs';
+
+//Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+
+conn.once('open', () => {
+    // Init Stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');//uploads is our collection name
+});
+
+
+//Create storage w/ multer gridfs
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            //crypto.randomBytes is used to generate names that is 16 characters long
+            crypto.randomBytes(16, (err, buf) => {
+                if (err){
+                    return reject(err);
+                }
+            const filename = buf.toString('hex') + path.extname(file.originalname);
+            const fileInfo = {
+                filename: filename,
+                bucketName: 'uploads'
+            };
+            resolve(fileInfo);
+
+            });
+        });
+    }
+});
+const upload = multer({storage})
+
+
+// mongoose.connect(DBURL, { useNewUrlParser: true }, (err, db) => {
+//     assert.equal(err, null);
+//     console.log('Successfully Connected to the Local Database.');
+// });
+
+
 
 
 app.get('/', (req, res) => {
